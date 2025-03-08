6864961f2a14101aa6ae2f662c80131e9f90197e
madsfjeldberg <mafj0001@stud.kea.dk>
2025-03-08 23:20:21 +0100

    more code cleanup, removed obsoletes

--- app.js
+++ app.js
@@ -1,53 +1,16 @@
 import express from "express";
 import path from "path";
-// import { processHtmlWithCodeHighlighting } from "./middleware/codeHighlighter.js";
 import proxy from "express-http-proxy";
 import ejsHighlighterMiddleware from "./middleware/ejsCodeHighlighter.js";
-import tocMiddleware from "./middleware/tocMiddleware.js";
 const app = express();
 const PORT = 8085;
 
 app.use(express.static(path.resolve("public")));
 app.use("/betterproxy", proxy("https://www.google.com"));
+app.use(ejsHighlighterMiddleware);
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("views"));
-
-app.use(ejsHighlighterMiddleware);
-app.use(tocMiddleware);
-
-// // Custom middleware to handle HTML responses with code highlighting
-// app.use(async (req, res, next) => {
-//   // Store the original sendFile method
-//   const originalSendFile = res.sendFile;
-
-//   // Override the sendFile method
-//   res.sendFile = async function (filePath, options, callback) {
-//     // Check if it's an HTML file
-//     if (filePath.endsWith(".html")) {
-//       try {
-//         // Process the HTML file with code highlighting
-//         const processedContent = await processHtmlWithCodeHighlighting(
-//           filePath
-//         );
-
-//         if (processedContent) {
-//           // Send the processed content
-//           res.setHeader("Content-Type", "text/html");
-//           return res.send(processedContent);
-//         }
-//       } catch (error) {
-//         console.error("Error processing HTML file:", error);
-//         // Fall back to the original sendFile method if there's an error
-//       }
-//     }
-
-//     // Call the original sendFile method if not an HTML file or if processing failed
-//     return originalSendFile.call(this, filePath, options, callback);
-//   };
-
-//   next();
-// });
 
 // Helper function to render content within the layout
 app.use((req, res, next) => {
